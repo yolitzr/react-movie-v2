1eb26175db7988deb3e2067288f0f21b4039555f
Yolit Zacarias <yolit.zr@gmail.com>
2022-09-04 18:13:18 -0300

    Add react-query

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body className="font-poppins">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
